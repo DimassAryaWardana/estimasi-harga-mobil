cc117634ac5639db8f76e96b91b244c67fac628e
DimassAryaWardana <dimasaryawardana10@gmail.com>
2026-01-05 23:20:42 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -17,5 +17,5 @@ if st.button('Estimasi Harga'):
     input_data = np.array([[year, mileage, tax, mpg, engineSize]])
     predict = model.predict(input_data)
 
-    st.success(f"Estimasi Harga Mobil Bekas (Pounds): {predict[0]:,.2f}")
-    st.success(f"Estimasi Harga Mobil Bekas dalam IDR (Juta): {(predict[0]*22000)/1_000_000:.2f}")
+    st.write(f"Estimasi Harga Mobil Bekas (Pounds): {predict[0]:,.2f}")
+    st.write(f"Estimasi Harga Mobil Bekas dalam IDR (Juta): {(predict[0]*22000)/1_000_000:.2f}")
